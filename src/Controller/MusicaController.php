e42a2e5b327513e4be9060773debccf85f7580aa
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-11-02 08:39:09 +0100

    App casi terminada

--- src/Controller/MusicaController.php
+++ src/Controller/MusicaController.php
@@ -18,14 +18,8 @@ class MusicaController extends AbstractController{
         8 => ["nombre" => "disco3", "precio" => 24, "autor_id" => 1],
         9 => ["nombre" => "disco8", "precio" => 89, "autor_id" => 1]
     ];  
-
-    //Mostrar todas las musicas
-    #[Route('/musica/mostrar/todo', name: 'mostrar_todo_user')]
-    public function mostrarTodo(){
-
-    }
     
-    //Insertar nueva musica mediante FORM
+    //Insertar nueva musica mediante formulario
     #[Route('/musica/nuevo', name: 'nueva_musica')]
     public function nuevo(ManagerRegistry $doctrine, Request $request) {
         $contacto = new Musica();
@@ -43,35 +37,40 @@ class MusicaController extends AbstractController{
         ));
     }
 
-    //Editar una musica
-    #[Route('/user/editar/{codigo}', name: 'editar_user', requirements:["codigo"=>"\d+"])]
+    //Mostrar todas las musicas
+    #[Route('/musica/mostrar/todo', name: 'mostrar_todo_user')]
+    public function mostrarTodo(){
+
+    }    
+
+    //Editar una musica, abriendo un formulario segun la ID de la musica elegida 
+    #[Route('/musica/editar/{codigo}', name: 'editar_musica', requirements:["codigo"=>"\d+"])]
     public function editar(ManagerRegistry $doctrine, Request $request, $codigo) {
-        $repositorio = $doctrine->getRepository(Contacto::class);
+        $repositorio = $doctrine->getRepository(Musica::class);
 
-        $contacto = $repositorio->find($codigo);
-        if ($contacto){
-            $formulario = $this->createForm(ContactoType::class, $contacto);
-
+        $musica = $repositorio->find($codigo);
+        if ($musica){
+            $formulario = $this->createForm(ContactoType::class, $musica);
             $formulario->handleRequest($request);
 
             if ($formulario->isSubmitted() && $formulario->isValid()) {
-                $contacto = $formulario->getData();
+                $musica = $formulario->getData();
                 $entityManager = $doctrine->getManager();
-                $entityManager->persist($contacto);
+                $entityManager->persist($musica);
                 $entityManager->flush();
-                return $this->redirectToRoute('ficha_contacto', ["codigo" => $contacto->getId()]);
+                return $this->redirectToRoute('ficha_musica', ["codigo" => $musica->getId()]);
             }
             return $this->render('nuevo.html.twig', array(
                 'formulario' => $formulario->createView()
             ));
         }else{
-            return $this->render('ficha_contacto.html.twig', [
-                'contacto' => NULL
+            return $this->render('ficha_musica.html.twig', [
+                'musica' => NULL
             ]);
         }
     }
     
-    //Insertar nueva musica, segun el array de arriba, con autor. http://127.0.0.1:8080/musica/insertar
+    //Insertar nueva musica, segun el array de arriba, con autor Prueba: http://127.0.0.1:8080/musica/insertar
     #[Route('/musica/insertar', name: 'insertar_musica')]
     public function insertar(ManagerRegistry $doctrine)
     {
@@ -93,7 +92,7 @@ class MusicaController extends AbstractController{
         }  
     }
     
-    //Buscar musica segun texto, muestra varias musicas http://127.0.0.1:8080/musica/buscar/disco
+    //Buscar musica segun texto, muestra varias musicas Prueba: http://127.0.0.1:8080/musica/buscar/disco
     #[Route('/musica/buscar/{texto}', name: 'buscar_musica')]
     public function buscar(ManagerRegistry $doctrine, $texto): Response{
         //Filtramos aquellos que contengan dicho texto en el nombre
@@ -106,7 +105,7 @@ class MusicaController extends AbstractController{
         ]);        
     }
 
-    //Buscar musica segun la ID http://127.0.0.1:8080/musica/2
+    //Buscar musica segun la ID Prueba: http://127.0.0.1:8080/musica/2
     #[Route('/musica/{codigo}', name: 'ficha_musica')]
     public function ficha(ManagerRegistry $doctrine, $codigo): Response{
 	    $repositorio = $doctrine->getRepository(Musica::class);
@@ -117,7 +116,7 @@ class MusicaController extends AbstractController{
 	    ]);
 	}    
     
-    //Modificar el nombre de la musica segun la ID http://127.0.0.1:8080/musica/update/1/disco3
+    //Modificar el nombre de la musica segun la ID Prueba: http://127.0.0.1:8080/musica/update/1/disco3
     #[Route('/musica/update/{id}/{nombre}', name: 'modificar_musica')]
     public function update(ManagerRegistry $doctrine, $id, $nombre): Response{
         $entityManager = $doctrine->getManager();
@@ -137,5 +136,26 @@ class MusicaController extends AbstractController{
             return $this->render('search/ficha_musica.html.twig', [
                 'musica' => null
             ]);
+    }
+
+    //Eliminar cualquier musica segun la ID  Prueba: http://127.0.0.1:8080/musica/delete/1
+    #[Route('/musica/delete/{id}', name: 'eliminar_musica')]
+    public function delete(ManagerRegistry $doctrine, $id): Response{
+        $entityManager = $doctrine->getManager();
+        $repositorio = $doctrine->getRepository(Musica::class);
+        $musica = $repositorio->find($id);
+        if ($musica){           
+            try
+            {
+                $entityManager->remove($musica);
+                $entityManager->flush();
+                return new Response("Musica eliminada");
+            } catch (\Exception $e) {
+                return new Response("Error eliminado objeto");
+            }  
+        }else
+            return $this->render('ficha_contacto.html.twig', [
+                'musica' => null
+            ]);  
     }
 }
